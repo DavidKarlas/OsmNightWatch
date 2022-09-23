44ba095e1ff15c38d7452942cfb02e429c39ab7a
Milica Jovanovic <jovanovicpmilica@gmail.com>
2022-09-23 19:58:59 +0200

    Update IssuesUploader

--- OsmNightWatch/IssuesUploader.cs
+++ OsmNightWatch/IssuesUploader.cs
@@ -1,9 +1,21 @@
 ﻿using OsmNightWatch.Lib;
+using System.Text.Json;
+using Azure.Storage.Blobs;
 
 internal class IssuesUploader
 {
     internal static void Upload(IssuesData data)
     {
-        
+        string json = JsonSerializer.Serialize(data);
+        var bytes = System.Text.Encoding.UTF8.GetBytes(json);
+        var memStream = new MemoryStream(bytes);
+
+        string blobStorageConnectionString = "";
+        string blobStorageContainerName = "data";
+        string fileName = "issues.json";
+
+        BlobContainerClient containerClient = new(blobStorageConnectionString, blobStorageContainerName);
+        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+        blobClient.UploadAsync(memStream, overwrite: true);
     }
 }
