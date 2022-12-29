e611c3692c2b4f3ac21ea11ab5d218c3124a8f47
David Karlaš <david.karlas@gmail.com>
2022-12-29 15:12:10 +0100

    Add RelationAnalyzer and JOSM links

--- OsmNightWatch.Web/Helper.cs
+++ OsmNightWatch.Web/Helper.cs
@@ -3,12 +3,26 @@
     public class Helper
     {
         public static List<(string issueType, string title, string pageUrl)> IssueTypes = new() {
-            ("OpenAdminPolygon","Open admins level 0-6.", "/OpenAdminPolygons" ),
-            ("OpenAdminPolygon7", "Open admins level 7.","/OpenAdminPolygons/7" ),
-            ("OpenAdminPolygon8","Open admins level 8.", "/OpenAdminPolygons/8" ),
+            ("OpenAdminPolygon","Open admins level 0-6", "/OpenAdminPolygons" ),
+            ("OpenAdminPolygon7", "Open admins level 7","/OpenAdminPolygons/7" ),
+            ("OpenAdminPolygon8","Open admins level 8", "/OpenAdminPolygons/8" ),
             //("OpenAdminPolygon9","Open  admins level 9.", "/OpenAdminPolygons/9" ),
             //("OpenAdminPolygon10", "Open  admins level 10.","/OpenAdminPolygons/10" ),
             ("BrokenCoastLine", "Broken coastlines","/BrokenWaterCoastlines" )
         };
+
+        public static string ConvertToFullOsmType(string osmType)
+        {
+            switch(osmType)
+            {
+                case "N":
+                    return "node";
+                case "W":
+                    return "way";
+                case "R":
+                    return "relation";
+            }
+            throw new ArgumentException(osmType);
+        }
     }
 }
---
+++ OsmNightWatch.Web/wwwroot/scripts/CallJosm.js
@@ -0,0 +1,6 @@
+﻿var CallJosm = CallJosm || {};
+CallJosm.Invoke = function (url) {
+    var oReq = new XMLHttpRequest();
+    oReq.open("get", url, true);
+    oReq.send();
+};
